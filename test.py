f1c71df9c13efee2ef433ec6018baf5c03b39243
matthewkim-dev <matthewkim04.dev@gmail.com>
2024-04-02 23:40:54 +0900

    fixed: import error

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
 from korea_news_crawler.articlecrawler import ArticleCrawler
 
-keyword = input('키워드를 입력하시오: ')
-Crawler = ArticleCrawler()
-Crawler.set_category('정치')
-Crawler.set_date_range('2024-03-01', '2024-03-31')
-Crawler.start(keyword)
+if __name__ == '__main__':
+	keyword = input('키워드를 입력하시오: ')
+	Crawler = ArticleCrawler()
+	Crawler.set_category('정치')
+	Crawler.set_date_range('2024-03-01', '2024-03-31')
+	Crawler.start(keyword)
